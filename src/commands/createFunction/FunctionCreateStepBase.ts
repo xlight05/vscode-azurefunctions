71af8651867b480c1a247ebd23920b9eb3074741
MicroFish91 <mwfisher91@gmail.com>
2022-10-24 16:25:27 -0700

    small fix

--- src/commands/createFunction/FunctionCreateStepBase.ts
+++ src/commands/createFunction/FunctionCreateStepBase.ts
@@ -100,7 +100,7 @@ export abstract class FunctionCreateStepBase<T extends IFunctionWizardContext> e
             }
 
             await AzExtFsExtra.writeJSON(hostJsonPath, hostJson);
-        } catch (error) {
+        } catch {
             ext.outputChannel.appendLog(hostJsonConfigFailed);
         }
     }
