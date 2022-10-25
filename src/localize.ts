2701f940d373f6d189f1a089172307cec3c92c45
MicroFish91 <mwfisher91@gmail.com>
2022-10-25 14:36:00 -0700

    update invalidLength string generator

--- src/localize.ts
+++ src/localize.ts
@@ -21,9 +21,14 @@ export const invalidAlphanumericWithHyphens: string = localize('invalidAlphanume
 export const invalidLowerCaseAlphanumericWithHyphens: string = localize('invalidLowerAlphanumericOrHyphen', `A name must consist of lower-case alphanumeric characters or '-', and must start and end with a lower-case alphanumeric character.`);
 export const overwriteRemoteConnection = (a: string) => localize('overwriteRemoteConnection', 'We detected a different local connection setting for "{0}" than what was previously used. Would you like to overwrite your remote setting?', a);
 
-export const invalidLength = (lowerLimit?: string, upperLimit?: string) => {
-    if (!lowerLimit || !upperLimit) {
-        return localize('invalidDefaultLength', 'A value is required to proceed.')
+export const invalidLength = (lowerLimitIncl?: string, upperLimitIncl?: string) => {
+    if (!lowerLimitIncl && !upperLimitIncl) {
+        return localize('invalidInputLength', 'A value is required to proceed.');
+    } else if (lowerLimitIncl && !upperLimitIncl) {
+        return localize('inputLengthTooShort', 'The value must be {0} or greater.', lowerLimitIncl);
+    } else if (!lowerLimitIncl && upperLimitIncl) {
+        return localize('inputLengthTooLong', 'The value must be {0} or less.', upperLimitIncl);
+    } else {
+        return localize('invalidBetweenInputLength', 'The value must be between {0} and {1} characters long.', lowerLimitIncl, upperLimitIncl);
     }
-    return localize('invalidBetweenLength', 'The value must be between {0} and {1} characters long.', lowerLimit, upperLimit)
 }
